d57e1ecb3ce9239f3f7dd52fa17888de684d28e3
Andra-sun <camileguimaraes463@gmail.com>
2025-09-15 09:51:55 -0300

    refactor(i18n): atualiza i18n para inglês padrão

--- src/i18n.js
+++ src/i18n.js
@@ -4,10 +4,12 @@ import translationEN from "./locales/en/translation.json";
 import translationPT from "./locales/pt/translation.json";
 import projetosEN from "./locales/en/projects/projetos.json";
 import projetosPT from "./locales/pt/projects/projetos.json";
+import navPT from "./locales/pt/nav/nav.json";
 import aboutPT from "./locales/pt/about/about.json";
 import aboutEN from "./locales/en/about/about.json";
 import certificadosEN from "./locales/en/certificates/certificados.json";
 import certificadosPT from "./locales/pt/certificates/certificados.json";
+import navEN from "./locales/en/nav/nav.json";
 
 const resources = {
     en: {
@@ -17,6 +19,7 @@ const resources = {
         },
         about: aboutEN,
         certificados: certificadosEN,
+        nav: navEN
     },
     pt: {
         translation: translationPT,
@@ -25,14 +28,15 @@ const resources = {
         },
         about: aboutPT,
         certificados: certificadosPT,
+        nav: navPT
     },
 };
 
 i18n.use(initReactI18next).init({
     resources,
-    lng: "pt",
-    fallbackLng: "pt",
-    ns: ["translation", "projects", "about", "certificados"],
+    lng: "en",
+    fallbackLng: "en",
+    ns: ["translation", "projects", "about", "certificados", "nav"],
     defaultNS: "translation",
     interpolation: {
         escapeValue: false,
